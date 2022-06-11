d9309b44c9d235ed0112ec280dfe4d81ab9ef9db
Ducklett <ivy16203@gmail.com>
2022-06-11 10:46:10 +0200

    fix compile script

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 
 tcc -run bake.c
-call gcc -O3 -s -mwindows -Ivendor -Isrc -Lvendor src/main.c icon.res -o -luser32 -lgdi32 -lopengl32 -o tivi.exe
+call gcc -O3 -s -mwindows -Ivendor -Isrc -Lvendor src/compilation.c icon.res -o -luser32 -lgdi32 -lopengl32 -o tivi.exe
